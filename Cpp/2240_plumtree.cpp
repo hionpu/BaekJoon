814ee3803a1df37e070e6a5a0f3eed62bd91678d
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2024-11-19 17:30:39 +0900

    ba

--- Cpp/2240_plumtree.cpp
+++ Cpp/2240_plumtree.cpp
@@ -1,16 +1,40 @@
 #include <iostream>   
 
 using namespace std;
-int t[1001];
-int w[30];
+int dp[1001][31];
+int plum[1001];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
+	int T, W;
+	cin >> T >> W;
+	for (int i = 1; i <= T; i++)
+	{
+		cin >> plum[i];
+	}
 
-	int t, w;
-	cin >> t >> w;
+	dp[1][0] = plum[1] == 1 ? 1 : 0;
+	dp[1][1] = plum[1] == 2 ? 1 : 0;
+	for (int t = 2; t <= T; t++)
+	{
+		for (int w = 0; w <= W; w++)
+		{
+			int currentMax = 0;
+			for (int k = 0; k <= w; k++)
+			{
+				currentMax = max(dp[t - 1][k], currentMax)  ;
+			}
+			dp[t][w] = currentMax + (plum[t] - 1 == w % 2 ? 1 : 0);
+		}
+	}
 
+	int ans = 0;
+	for (int i = 0; i <= W; i++)
+	{
+		ans = max(ans, dp[T][i]);
+	}
+	cout << ans << '\n';
 	return 0;
 }
 
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+1. Cpp.exe 못찾는다고 나오면 모든 cpp 파일이 excluded from build == Yes 라서 그런것
+2.
